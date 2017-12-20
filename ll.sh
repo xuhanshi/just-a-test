cadbdafc13a94a1903eca43cb4b1a6e18eb4e6b3
xuhanshi <hsxu@medlab.com>
2017-12-20 18:09:15 +0800

    add ll.sh

---
+++ ll.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+function usage
+{
+cat $usage << EOF
+#================================================================
+#   filename:ll.sh
+#   author:Hanshi Xu
+#   created time:2017.12.20
+#   description:
+#   example:
+#================================================================
+EOF
+}
+echo "test for test"
